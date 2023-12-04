1ba63da6e2ccf4d24c6a378c5d59649c919dc728
kearnsform <80419983+kearnsform@users.noreply.github.com>
2023-12-04 13:29:07 -0700

    second attempt

--- 2023/day3/day3.py
+++ 2023/day3/day3.py
@@ -1,50 +1,81 @@
 #part 1: 527446
 
-
 import re
+from enum import Enum
 
 symbols = '*-@/+%$=&#'
-symbol_pattern = '[\\' + '|\\'.join([c for c in symbols]) + ']'
+member_pattern = r'\d+|' + '\\' + '|\\'.join([c for c in symbols])
 input_file_part1 = 'input.txt'
+file_size = 140
 
-def get_symbol_map():
-	symbol_map = {}
+class MemberType(Enum):
+	NUMBER = 'number'
+	SYMBOL = 'symbol'
+
+class FileMember():
+	def __init__(self, line_num, start, end, value, member_lookup):
+		self.type = MemberType.NUMBER if re.match(r'\d+', value) else MemberType.SYMBOL
+		self.coord = (line_num, (start, end))
+		self.value = int(value) if self.type == MemberType.NUMBER else value
+		self.member_lookup = member_lookup
+
+	def get_adjacents(self):
+		adjacents = set()
+		lines_to_check = list(range(max(self.coord[0] - 1, 1), min(self.coord[0] + 2, file_size + 1)))
+		idxs_to_check = list(range(max(self.coord[1][0] - 1, 0), min(self.coord[1][1] + 2, file_size + 1)))
+		for line_num in lines_to_check:
+			for idx in idxs_to_check:
+				member = self.member_lookup.get_member(line_num, idx)
+				if member and member != self:
+					adjacents.add(member)
+		return adjacents
+
+	def __repr__(self):
+		return f'{self.type}, {self.coord}, value={self.value}'
+
+class MemberLookup():
+	def __init__(self):
+		self.lookup = {}
+
+	def add(self, member):
+		self.lookup[member.coord[0]] = self.lookup.get(member.coord[0], {}) | {member.coord[1][0]: member}
+
+	def get_member(self, line_num, idx):
+		nearest = self.get_nearest(line_num, idx)
+		member_found = None
+		if nearest and idx in range(nearest.coord[1][0], nearest.coord[1][1] + 1):
+			return nearest
+		return member_found
+
+	def get_nearest(self, line_num, coord):
+		keys = sorted(list(self.lookup.get(line_num, {}).keys()))
+		max_idx = len(keys) - 1
+		for idx, key in enumerate(keys):
+			next_idx = min(max_idx, idx + 1)
+			next_key = keys[next_idx]
+			if idx == max_idx:
+				next_key = file_size
+			if key <= coord and coord < next_key:
+				return self.lookup[line_num][key]
+		return None
+
+def populate_lookup(lookup):
 	line_num = 0
 	with open(input_file_part1) as file:
 		lines = file.readlines()
 		for line in lines:
 			line_num += 1
-			symbol_map[line_num] = [(m.start(0)) for m in re.finditer(symbol_pattern, line)]	
-	return symbol_map
-
-def get_numbers_positions(line):
-	return [(int(m.group()), (m.start(0), m.end(0)-1)) for m in re.finditer(r'\d+', line)]
-
-def adjacent_symbol(line_number, num_pos, symbol_map):
-	lines_to_check = list(range(max(line_number - 1, 1), min(line_number + 2, 140)))
-	for lin_num in lines_to_check:
-		for sym_loc in symbol_map.get(lin_num, []):
-			if sym_loc in range(num_pos[0] - 1, num_pos[1] + 2):
-				return True
-	return False
-
-def get_sum(line_number, line, symbol_map):
-	np = get_numbers_positions(line)
-	sum = 0
-	for n, p in np:
-		if adjacent_symbol(line_number, p, symbol_map):
-			sum += n
-	return sum
-
+			members = [(m.group(), (m.start(0), m.end(0)-1)) for m in re.finditer(member_pattern, line)]	
+			for member in members:
+				lookup.add(FileMember(line_num, member[1][0], member[1][1], member[0], lookup))
 
 if __name__ == '__main__':
 	#part 1
-	symbol_map = get_symbol_map()
-	lin_num = 0
-	total_sum = 0
-	with open(input_file_part1) as file:
-		lines = file.readlines()
-		for line in lines:
-			lin_num += 1
-			total_sum += get_sum(lin_num, line, symbol_map)
-	print(total_sum)
+	member_lookup = MemberLookup()
+	populate_lookup(member_lookup)
+	sum = 0
+	for line_num, members in member_lookup.lookup.items():
+		for idx, member in members.items():
+			if member.type == MemberType.NUMBER and any([adj.type == MemberType.SYMBOL for adj in member.get_adjacents()]):
+				sum += member.value
+	print(sum)
