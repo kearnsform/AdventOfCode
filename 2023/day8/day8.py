4d858490b6c534b069b501574ed9dd10e41434d2
Erron Kearns <ekearns@visitpay.com>
2023-12-08 13:41:25 -0700

    update day8 for part 2

--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -20,9 +20,9 @@ class DesertMap():
         m_split = [e for e in re.split('[ |=|,|(|)]', mapping_str) if e != '']
         self.lookup[m_split[0]] = (m_split[1], m_split[2])
 
-    def get(self, key, instruction):
+    def get(self, nodes, instruction):
         i = 0 if (instruction == 'L') else 1
-        return self.lookup[key][i]
+        return [self.lookup[node][i] for node in nodes]
 
 
 with open('input.txt', 'r') as file:
@@ -32,8 +32,11 @@ with open('input.txt', 'r') as file:
     for line in file.readlines():
         desert_map.add_entry(line)
 
-key = 'AAA'
-while key != 'ZZZ':
-    key = desert_map.get(key, instructions.next())
+def all_end_in_z(nodes):
+    return all([node[-1] == 'Z' for node in nodes])
+
+nodes = [node for node in desert_map.lookup.keys() if node[-1] == 'A']
+while not all_end_in_z(nodes):
+    nodes = desert_map.get(nodes, instructions.next())
 
 print(instructions.step_count)
